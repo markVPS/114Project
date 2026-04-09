4466db06904123b5ed00b0ebd894b9f514c18360
kiraoliver333-blip <kiraoliver333@gmail.com>
2026-04-08 18:32:46 -0700

    Update MemoryManager.h

--- include/MemoryManager.h
+++ include/MemoryManager.h
@@ -2,7 +2,10 @@
 #define MEMORY_MANAGER_H
 
 #include <vector>
-
+//region of memory shared. 
+//start = the starting address of this block in the simulated memory space
+//size = length of block
+//free = true if block is available for mem allocation, false if already allocated
 struct MemoryBlock {
     int start;
     int size;
@@ -10,15 +13,18 @@ struct MemoryBlock {
 };
 
 class MemoryManager {
+// total size of the memory space
+//vector is the list of memory blocks. allocated and free blocks
 private:
     int totalMemory;
     std::vector<MemoryBlock> blocks;
 
 public:
     MemoryManager(int totalMemory = 1024);
-
+// try to allocate a block of 'size', returns true and sets startAddress to the address of allocated region
     bool allocate(int size, int& startAddress);
     void freeBlock(int startAddress);
+//helps reduce fragementation
     void mergeFreeBlocks();
 
     const std::vector<MemoryBlock>& getBlocks() const;
